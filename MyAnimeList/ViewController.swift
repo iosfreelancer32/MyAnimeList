27fe4880954305ebb5acdf72742ee321c360fe21
iosfreelancer32 <iosfreelancer32@gmail.com>
2020-12-01 21:36:21 +0530

    Initial Commit

---
+++ MyAnimeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAnimeList
+//
+//  Created by Rajkumar Gurunathan on 01/12/20.
+//  Copyright © 2020 Rajkumar Gurunathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
